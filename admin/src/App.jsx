e523bf2e5b987cafca14062eecf1fe649c07104c
Mohammad Hafeez <syntaxhafeez@gmail.com>
2024-12-23 23:46:03 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "https://foodify-backend-7hnv.onrender.com/"
+  const url = "https://foodify-backend-7hnv.onrender.com"
 
   return (
     <div>
