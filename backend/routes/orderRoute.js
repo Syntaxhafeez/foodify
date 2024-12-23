f37d0fab35f57d6387a308c4d346dc5dcefbb030
Syntaxhafeez <syntaxhafeez@gmail.com>
2024-12-23 23:21:07 +0530

    updated

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -9,7 +9,7 @@ const orderRouter = express.Router();
 orderRouter.post("/place", authMiddleware, placeOrder);
 orderRouter.post("/verify", verifyOrder)
 orderRouter.post("/userorders", authMiddleware, userOrders)
-orderRouter.get("/api/food/list", listOrders)
+orderRouter.get("/list", listOrders)
 orderRouter.post("/status", updateStatus)
 
 
