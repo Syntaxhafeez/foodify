b2f5dd2ef9c1e61eedea0687eb21816ed3863654
Syntaxhafeez <syntaxhafeez@gmail.com>
2024-12-24 00:34:36 +0530

    updated-pages

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import Sidebar from './components/sidebar/Sidebar'
-import { Route, Routes } from 'react-router-dom'
-import Add from './pages/Add/Add'
-import List from './pages/List/List'
-import Orders from './pages/Orders/Orders'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Add from "./pages/Add/Add";
+import List from "./pages/List/List";
+import Orders from "./pages/Orders/Orders";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-
-  const url = "https://foodify-backend-7hnv.onrender.com";
-
+  const url = "https://foodify-backend-7hnv.onrender.com/";
   return (
     <div>
       <ToastContainer />
@@ -20,13 +18,13 @@ const App = () => {
       <div className="app-content">
         <Sidebar />
         <Routes>
-          <Route path='/add' element={<Add url={url} />} />
-          <Route path='/list' element={<List url={url} />} />
-          <Route path='/orders' element={<Orders url={url} />} />
+          <Route path="/add" element={<Add url={url}/>} />
+          <Route path="/list" element={<List url={url}/>} />
+          <Route path="/orders" element={<Orders url={url}/>} />
         </Routes>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
