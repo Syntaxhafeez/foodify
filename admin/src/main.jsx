a17daac3af48773cf60c1f9283a29505098121c9
Syntaxhafeez <syntaxhafeez@gmail.com>
2024-12-24 00:55:54 +0530

    updated

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,14 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import StoreContextProvider from "./context/StoreContext.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from "react-router-dom"
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <StoreContextProvider>
-      <App />
-    </StoreContextProvider>
+    <App />
   </BrowserRouter>
-);
+)
